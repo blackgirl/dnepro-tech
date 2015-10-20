49877a753b728d14476b9305f62972a45bb88f10
Alyona <a.chornaya@gmail.com>
2015-10-20 08:14:55 +0300

    moved dark site version to the root dir

--- js/script.js
+++ js/script.js
@@ -2,13 +2,14 @@
     $(function () {
 
       // Slideshow 1
-      $("#slider1").responsiveSlides({
-        speed: 1200
-      });
-	  
+      // $("#slider1").responsiveSlides({
+      //   speed: 1200
+      // });
+	  $.scrollIt();
 	  
 	$j=jQuery.noConflict();
-$j(document).ready(function() {	
+$j(document).ready(function() {
+	
 	var nav = $j('#logo');
 	$j(window).scroll(function () {
 		if ($j(this).scrollTop() > 380) {
